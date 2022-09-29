b8e42fe75a26869baa32531cfbb782021fb3ca9b
Piyumi Chan Mahaarachchi <76487449+YumeChaan@users.noreply.github.com>
2022-09-29 23:03:07 +0530

    fire event

---
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -0,0 +1,44 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import AddInput from '../AddInput';
+
+const mockedSetTodos = jest.fn();
+
+describe("AddInput", () => {
+    it('should render input element', async() => {
+        render(
+            <AddInput 
+              todos={[]}
+              setTodos={mockedSetTodos}
+            />
+        );
+        const inputElement = screen.getByPlaceholderText(/add a new task here.../i);
+        expect(inputElement).toBeInTheDocument();
+    });
+})
+
+it('should be able type in input', async() => {
+    render(
+        <AddInput 
+          todos={[]}
+          setTodos={mockedSetTodos}
+        />
+    );
+    const inputElement = screen.getByPlaceholderText(/add a new task here.../i);
+    fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping" } });
+    expect(inputElement.value).toBe("Go Grocery Shopping");
+});
+
+it('should have empty input when add button is clicked', async() => {
+    render(
+        <AddInput 
+          todos={[]}
+          setTodos={mockedSetTodos}
+        />
+    );
+    const inputElement = screen.getByPlaceholderText(/add a new task here.../i);
+    const buttonElement = screen.getByRole("button", { name: /add/i });
+    fireEvent.change(inputElement, { target: { value: "Go Grocery Shopping" } });
+    fireEvent.click(buttonElement);
+    expect(inputElement.value).toBe("");
+});
+
